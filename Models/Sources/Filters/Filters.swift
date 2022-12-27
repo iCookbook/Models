84bbfe9371fd44e9b23465d4563eb62ac3c97b27
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-12-27 23:21:55 +0300

    feat: create `Filters` enum

---
+++ Models/Sources/Filters/Filters.swift
@@ -0,0 +1,13 @@
+//
+//  Filters.swift
+//  Models
+//
+//  Created by Егор Бадмаев on 27.12.2022.
+//
+
+public enum Filters: Int {
+    case diet
+    case cuisine
+    case dish
+    case meal
+}
